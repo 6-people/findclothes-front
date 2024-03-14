f5db1ca062148e8524bff3c3649ece29f0432da0
정은하 <97080242+jeongeunhq@users.noreply.github.com>
2024-03-14 21:42:38 +0900

    Update NicknameChange.js

--- src/NicknameChange.js
+++ src/NicknameChange.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-=======
-import React, {useEffect, useState} from 'react';
-import {Link} from 'react-router-dom';
->>>>>>> 7833f41aaa87d86056b2db23b44b309751b2978d
 import backIcon from './icons/back.png';
 import mypage from './icons/user.png';
 import community from './icons/community.png';
@@ -13,23 +8,14 @@ import home from './icons/home.png';
 import product from './icons/product.png';
 import './css/common.css';
 
-<<<<<<< HEAD
 const NicknameChange = () => {
     const [currentNickname, setCurrentNickname] = useState('닉네임1'); 
     const [newNickname, setNewNickname] = useState(''); 
-=======
-const NicknameChange = ({currentNickname, onNicknameChange}) => {
-    const [newNickname, setNewNickname] = useState('닉네임1');
->>>>>>> 7833f41aaa87d86056b2db23b44b309751b2978d
     const [name, setName] = useState('이름1');
     const [id, setId] = useState('아이디1');
 
     const handleNicknameChange = () => {
-<<<<<<< HEAD
         setCurrentNickname(newNickname);
-=======
-        onNicknameChange(newNickname);
->>>>>>> 7833f41aaa87d86056b2db23b44b309751b2978d
         setNewNickname('');
     };
 
@@ -38,11 +24,7 @@ const NicknameChange = ({currentNickname, onNicknameChange}) => {
             <div className='centerXWrapper'>
                 <div className='pageNameBox'>
                     <Link to="/inform">
-<<<<<<< HEAD
                         <img src={backIcon} alt="Back Icon" className="back-icon" />
-=======
-                        <img src={backIcon} alt="Back Icon" className="back-icon"/>
->>>>>>> 7833f41aaa87d86056b2db23b44b309751b2978d
                     </Link>
                     닉네임 변경
                     <div className='imgBlank'></div>
@@ -51,11 +33,7 @@ const NicknameChange = ({currentNickname, onNicknameChange}) => {
             <div className='centerXWrapper'>
                 <div>
                     <div className='userInfoCard'>
-<<<<<<< HEAD
                         <p className='userInfoCardNickname'>{newNickname || currentNickname}</p>
-=======
-                        <p className='userInfoCardNickname'>{newNickname}</p>
->>>>>>> 7833f41aaa87d86056b2db23b44b309751b2978d
                         <p className='userInfoCardId'>{id}</p>
                     </div>
                     <div className='userInfoInput'>
@@ -63,14 +41,9 @@ const NicknameChange = ({currentNickname, onNicknameChange}) => {
                             <label>
                                 <input
                                     type="text"
-<<<<<<< HEAD
                                     placeholder="닉네임"
                                     onChange={(e) => setNewNickname(e.target.value)}
                                     value={newNickname}
-=======
-                                    placeholder="기존 닉네임"
-                                    onChange={(e) => setNewNickname(e.target.value)}
->>>>>>> 7833f41aaa87d86056b2db23b44b309751b2978d
                                 />
                             </label>
                         </div>
@@ -80,21 +53,12 @@ const NicknameChange = ({currentNickname, onNicknameChange}) => {
             </div>
             <div className='centerXWrapper'>
                 <div className='move'>
-<<<<<<< HEAD
                     <img src={product} alt="product" className="product-icon" />
                     <img src={digging} alt="digging" className="digging-icon" />
                     <img src={home} alt="home" className="home-icon" />
                     <img src={community} alt="community" className="community-icon" />
                     <Link to="/mypage">
                         <img src={mypage} alt="mypage" className="mypage-icon" />
-=======
-                    <img src={product} alt="product" className="product-icon"/>
-                    <img src={digging} alt="digging" className="digging-icon"/>
-                    <img src={home} alt="home" className="home-icon"/>
-                    <img src={community} alt="community" className="community-icon"/>
-                    <Link to="/mypage">
-                        <img src={mypage} alt="mypage" className="mypage-icon"/>
->>>>>>> 7833f41aaa87d86056b2db23b44b309751b2978d
                     </Link>
                 </div>
             </div>
@@ -103,9 +67,3 @@ const NicknameChange = ({currentNickname, onNicknameChange}) => {
 };
 
 export default NicknameChange;
-<<<<<<< HEAD
-=======
-
-// TODO : 닉네임 입력란을 다 지우면 유저 정보에서 닉네임 공간이 사라짐
-// TODO : js와 css 파일명을 카멜케이스, 파스칼케이스 둘 다 쓰고 있는데 명명규칙 서치 후 통일
->>>>>>> 7833f41aaa87d86056b2db23b44b309751b2978d
