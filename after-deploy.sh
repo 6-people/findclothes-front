1bf02449429806bddee6e2d6d915c8014d04ba9b
정은하 <97080242+jeongeunhq@users.noreply.github.com>
2024-05-05 03:44:52 +0900

    Update after-deploy.sh

--- after-deploy.sh
+++ after-deploy.sh
@@ -1,1 +1,6 @@
-/home/ubuntu/findclothes_deploy > ~/tempLog 2> ~/errorLog
+REPOSITORY=/home/ubuntu/findclothes_deploy
+
+if [ -d $REPOSITORY/app ]; then
+    rm -rf $REPOSITORY/app
+fi
+mkdir -vp $REPOSITORY/app
