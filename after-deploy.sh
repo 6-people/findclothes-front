866fb5a02cf9eec51b17c308f0c061d925a3bd87
정은하 <97080242+jeongeunhq@users.noreply.github.com>
2024-05-05 03:39:51 +0900

    Update after-deploy.sh

--- after-deploy.sh
+++ after-deploy.sh
@@ -1,6 +1,1 @@
-REPOSITORY=/home/ubuntu/findclothes_deploy
-
-cd $REPOSITORY  
-
-pm2 start npm --name findclothes_deploy -- run dev --watch
-pm2 save
+/home/ubuntu/findclothes_deploy > ~/tempLog 2> ~/errorLog
